ab3bfc42e95339beb8a082e62cf8c0c7dafb28be
robin singh <robinsingh1117@gmail.com>
2026-06-19 07:29:40 +0530

    I added comments for beter understanding of the code.

--- openID with backend/app.js
+++ openID with backend/app.js
@@ -19,48 +19,55 @@ app.use(
   }),
 );
 
+//Generating Url and Redirecting to it
 app.get("/auth/google", (req, res) => {
   let googleAutherizationUrl = generateAuthUrl()
-  // res.redirect(`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.GOOGLE_CLIENT_ID}&scope=openid email profile&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}`,);
   res.redirect(googleAutherizationUrl);
 });
 
-app.get("/get-code", async (req, res) => {
-  let code = req.query.code;
+//Extract Auth code and Exchange for id_token and after interacting with DB make redirect() to /callback
+app.get("/get-code", async (req, res, next) => {
+  try {
+    let code = req.query.code;
+    let { sub, email, name, picture } = await loginWithGoogle(code);
 
-  let { sub, email, name, picture } = await loginWithGoogle(code);
+    let existingSession = sessionDB.find(({ id, uid }) => uid == sub);
 
-  let existingSession = sessionDB.find(({ id, uid }) => uid == sub);
+    if (existingSession) {
 
-  if (existingSession) {
+      return res.redirect(
+        `http://127.0.0.1:5500/callback.html?sid=${existingSession.id}`,
+      );
+    }
 
-    return res.redirect(
-      `http://127.0.0.1:5500/callback.html?sid=${existingSession.id}`,
-    );
-  }
+    let existingUser = userDB.find(({ id }) => id === sub);
+    if (existingUser) {
+      let id = crypto.randomUUID();
+      sessionDB.push({ id, uid: sub });
+      await fs.writeFile("./sessionDB.json", JSON.stringify(sessionDB, null, 2));
+      return res.redirect(`http://127.0.0.1:5500/callback.html?sid=${id}`);
+    }
 
-  let existingUser = userDB.find(({ id }) => id === sub);
-  if (existingUser) {
+    let createdUser = { id: sub, email, name, picture };
+    userDB.push(createdUser);
+    await fs.writeFile("./userDB.json", JSON.stringify(userDB, null, 2));
+
     let id = crypto.randomUUID();
-    sessionDB.push({ id, uid: sub });
+
+    let createdSession = { id, uid: sub };
+    sessionDB.push(createdSession);
     await fs.writeFile("./sessionDB.json", JSON.stringify(sessionDB, null, 2));
-    return res.redirect(`http://127.0.0.1:5500/callback.html?sid=${id}`);
+
+
+    res.redirect(`http://127.0.0.1:5500/callback.html?sid=${id}`);
+  } catch (error) {
+    console.log('Error From getCode :', error.message);
+    next(error)
   }
 
-  let createdUser = { id: sub, email, name, picture };
-  userDB.push(createdUser);
-  await fs.writeFile("./userDB.json", JSON.stringify(userDB, null, 2));
-
-  let id = crypto.randomUUID();
-
-  let createdSession = { id, uid: sub };
-  sessionDB.push(createdSession);
-  await fs.writeFile("./sessionDB.json", JSON.stringify(sessionDB, null, 2));
-
-
-  res.redirect(`http://127.0.0.1:5500/callback.html?sid=${id}`);
 });
 
+//Setting sid Cookies to browser side.
 app.get("/set-session-cookie", (req, res) => {
   let { sid } = req.query;
   res.cookie("sid", sid, {
--- openID with backend/services/loginWithGoogle.js
+++ openID with backend/services/loginWithGoogle.js
@@ -5,12 +5,14 @@ const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const redirectUrl = process.env.GOOGLE_REDIRECT_URI;
 
+//setting-up Google client
 let client = new OAuth2Client({
     client_id: clientId,
     client_secret: clientSecret,
     redirectUri: redirectUrl
 })
 
+//generating auth Url 
 export function generateAuthUrl(params) {
     let generatedUri = client.generateAuthUrl({
         scope: ['profile', 'openid', 'email'],
@@ -21,12 +23,13 @@ export function generateAuthUrl(params) {
     return generatedUri;
 }
 
+//Extract User Information from the Token Received from Google's Token Endpoint
 export async function loginWithGoogle(code) {
     try {
 
         let result = await client.getToken(code)
         let data = result.tokens
-        console.log(data);
+        // console.log(data);
         
         if (data.error) {
             console.log('Error From loginWithGoogle :', data.error);
@@ -41,7 +44,7 @@ export async function loginWithGoogle(code) {
         return loginTicket.payload
 
     } catch (error) {
-        console.log('Error From loginWithGoogle :', error);
+        console.log('Error From loginWithGoogle :', error.message);
     }
 
 
